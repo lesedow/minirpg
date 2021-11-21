25d06db4a430d28f845c8edffa5f254f0ee48255
lesedow <lesedow@gmail.com>
2021-11-21 06:05:49 -0800

    added shop functionality

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
-use minirpg::Player;
+use minirpg::player::Player;
+use minirpg::weapon::Weapon;
+use minirpg::utils::*;
 
-fn execute_action(action: &str, player: &Player) {
+pub fn advance() {}
+
+pub fn shop(player: &mut Player) {
+    let shop_stock = get_stock().unwrap();
+
+    println!("Welcome to the shop adventurer!"); 
+
+    for (i, val) in shop_stock.iter().enumerate() {
+        println!("{}) {} - ${}", i, val.name, val.price);
+    }
+
+    loop {
+        let option = get_user_input().unwrap();
+        if let Some(num) = get_as_usize(&option) {
+            if num >= shop_stock.len() { 
+                println!("Invalid shop item!");
+                continue; 
+            }
+
+            let weapon = &shop_stock[num];
+            player.set_weapon(Weapon::new(&weapon.name, weapon.price, weapon.damage));
+
+        }
+    }
+
+}
+
+fn execute_action(action: &str, player: &mut Player) {
     match action.to_lowercase().trim() {
-        "advance" => minirpg::advance(),
-        "shop" => minirpg::shop(player),
+        "advance" => advance(),
+        "shop" => shop(player),
         _ => println!("Invalid option!")
     }
 }
@@ -15,8 +44,8 @@ fn main() {
     loop {
         if player.get_health() == 0 { break }
 
-        let user_input = minirpg::get_user_input().unwrap();
-        execute_action(&user_input, &player);
+        let user_input = get_user_input().unwrap();
+        execute_action(&user_input, &mut player);
 
     }
 }
